18072ede1bc2df2fb04b9ea6070cffeeb7f384f2
pappukr3631 <100488442+pappukr3631@users.noreply.github.com>
2023-07-04 17:18:10 +0530

    add student

--- script.js
+++ script.js
@@ -49,8 +49,8 @@ function appendDataToTable(data) {
     const degree_td = document.createElement("td");
     degree_td.innerHTML = `${data.degree}
     <div>
-        <img src="/img/edit1.png" onclick="edit(this)" />
-        <img src="/img/trash-2.png" onclick="deleteElement(this)" />
+        <img src="img/edit 1.svg" onclick="edit(this)" />
+        <img src="img/trash-2 1.svg" onclick="deleteElement(this)" />
     </div>`;
 
     tr.appendChild(id_td);
