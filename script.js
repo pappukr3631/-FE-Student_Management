9a6db88f5e65617c7175ca2896455fd3a8bf4380
pappukr3631 <100488442+pappukr3631@users.noreply.github.com>
2023-07-04 20:17:20 +0530

    edit and delete done -search functionality left

--- script.js
+++ script.js
@@ -4,25 +4,36 @@ let students = [
     { ID: 3, name: 'Charlie', age: 20, grade: 'C', degree: 'Arts', email: 'charlie@example.com' }
 ];
 
+// Show Default Data in Table
+showDefaultData();
+function showDefaultData() {
+    for(let i=0; i<students.length; i++) {
+        appendDataToTable(students[i]);
+    }
+}
 
-function getData() {
+// Add Student
+function addStudent() {
     const name = document.getElementById("name-input");
     const age = document.getElementById("age-input");
     const email = document.getElementById("email-input");
-    const gpa = document.getElementById("gpa-input");
+    const grade = document.getElementById("grade-input");
     const degree = document.getElementById("degree-input");
 
     let data = {
         ID: getMaxID() + 1,
         name: name.value,
         age: age.value,
-        grade: gpa.value,
+        grade: grade.value,
         degree: degree.value,
         email: email.value
     };
 
     students.push(data);
     appendDataToTable(data);
+
+    // Reset form
+    resetForm();
 }
 
 function appendDataToTable(data) {
@@ -43,8 +54,8 @@ function appendDataToTable(data) {
     const email_td = document.createElement("td");
     email_td.innerText = data.email;
 
-    const gpa_td = document.createElement("td");
-    gpa_td.innerText = data.grade;
+    const grade_td = document.createElement("td");
+    grade_td.innerText = data.grade;
 
     const degree_td = document.createElement("td");
     degree_td.innerHTML = `${data.degree}
@@ -57,7 +68,7 @@ function appendDataToTable(data) {
     tr.appendChild(name_td);
     tr.appendChild(email_td);
     tr.appendChild(age_td);
-    tr.appendChild(gpa_td);
+    tr.appendChild(grade_td);
     tr.appendChild(degree_td);
 
     tbody.appendChild(tr);
@@ -71,4 +82,107 @@ function getMaxID() {
             id = students[i].ID;
     }
     return id;
+}
+
+// Deleting a row
+function deleteElement(element) {
+    let row = element.closest("tr");
+    // Remove from array
+    let idToRemove = parseInt(row.cells[0].innerText);
+    deleteDataFromArray(idToRemove);
+    
+    // Removing from UI
+    row.remove();
+}
+
+function deleteDataFromArray(idToRemove) {
+    for(let i=0; i<students.length; i++) {
+        if(students[i].ID == idToRemove) {
+            students.splice(i, 1);
+            break;
+        }
+    }
+    console.log(students);
+}
+
+// Edit Student functionality
+let indexToEdit = -1;
+let rowToEdit = null;
+function edit(element) {
+    rowToEdit = element.closest("tr");
+
+    let idToEdit = parseInt(rowToEdit.cells[0].innerText);
+
+    fillForm(idToEdit);
+}
+
+function fillForm(id) {
+    let studentToEdit = null;
+    for(let i=0; i<students.length; i++) {
+        if(students[i].ID === id) {
+            indexToEdit = i;
+            studentToEdit = students[i];
+            break;
+        }
+    }
+    // Get elements
+    const name = document.getElementById("name-input");
+    const age = document.getElementById("age-input");
+    const email = document.getElementById("email-input");
+    const grade = document.getElementById("grade-input");
+    const degree = document.getElementById("degree-input");
+    const button = document.getElementById("submit");
+    // Put data
+    name.value = studentToEdit.name;
+    age.value = studentToEdit.age;
+    email.value = studentToEdit.email;
+    grade.value = studentToEdit.grade;
+    degree.value = studentToEdit.degree;
+
+    // Change Button value
+    button.innerText = "Edit Student";
+    button.onclick = editStudent;
+}
+
+function editStudent() {
+    console.log("edit student function");
+
+    // Get elements
+    const name = document.getElementById("name-input");
+    const age = document.getElementById("age-input");
+    const email = document.getElementById("email-input");
+    const grade = document.getElementById("grade-input");
+    const degree = document.getElementById("degree-input");
+    const button = document.getElementById("submit");
+    // Change values of selected student
+    rowToEdit.cells[1].innerText = name.value;
+    rowToEdit.cells[2].innerText = email.value;
+    rowToEdit.cells[3].innerText = age.value;
+    rowToEdit.cells[4].innerText = grade.value;
+    rowToEdit.cells[5].innerHTML = `${degree.value}
+    <div>
+        <img src="img/edit 1.svg" onclick="edit(this)" />
+        <img src="img/trash-2 1.svg" onclick="deleteElement(this)" />
+    </div>`;
+
+    // Change the students arr
+    students[indexToEdit].name = name.value;
+    students[indexToEdit].email = email.value;
+    students[indexToEdit].age = age.value;
+    students[indexToEdit].grade =grade.value;
+    students[indexToEdit].degree = degree.value;
+
+    console.log(students);
+
+    // Change Button value
+    button.innerText = "Add Student";
+    button.onclick = addStudent;
+
+    // clear the form
+    resetForm();
+}
+
+function resetForm() {
+    const form = document.getElementById("form");
+    form.reset();
 }
